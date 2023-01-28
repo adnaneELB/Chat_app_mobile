0e0004009e3acb1cc98adef8da21e17d59900e91
studyVersion <0314sbadre@e-itaca.es>
2023-01-28 23:23:53 +0100

    adnane you can now edit layout

--- app/src/main/java/com/example/application_chat/LoginActivity.java
+++ app/src/main/java/com/example/application_chat/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.application_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button registerBtn;
+    TextView signUp;
+    FirebaseAuth fireAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fireAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.loginButton);
+        signUp = findViewById(R.id.signupHere);
+
+        registerBtn.setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        });
     }
 }
--- app/src/main/java/com/example/application_chat/RegisterActivity.java
+++ app/src/main/java/com/example/application_chat/RegisterActivity.java
@@ -1,14 +1,73 @@
 package com.example.application_chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    EditText regEmail;
+    EditText regPassword;
+    Button registerBtn;
+    TextView signIn;
+    FirebaseAuth fireAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fireAuth = FirebaseAuth.getInstance();
+
+        regEmail = findViewById(R.id.email);
+        regPassword = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.registerBtn);
+        signIn = findViewById(R.id.signinHere);
+
+        registerBtn.setOnClickListener(view -> {
+            createUser();
+
+        });
+        signIn.setOnClickListener(View -> {
+            startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+        });
+    }
+
+    private void createUser(){
+        String email = regEmail.getText().toString();
+        String password = regPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            regEmail.setError("El correo electrónico no puede estar vacío");
+            regEmail.requestFocus();
+        }else if(TextUtils.isEmpty(password)){
+            regPassword.setError("La palabra clave no puede estar vacía");
+            regPassword.requestFocus();
+        }else{
+            fireAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    }
+                }
+            });
+        }
     }
 }
