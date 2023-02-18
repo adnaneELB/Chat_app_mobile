ef336a5724073d0403a2a38481418bfc718bfd65
studyVersion <0314sbadre@e-itaca.es>
2023-02-18 17:07:13 +0100

    QR code added

--- app/src/main/java/com/example/fragments/QrFragment.java
+++ app/src/main/java/com/example/fragments/QrFragment.java
@@ -1,5 +1,9 @@
 package com.example.fragments;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +11,73 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.application_chat.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QrFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class QrFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class QrFragment extends Fragment  {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        private TextView scannedTextView;
+        private Button scan;
 
-    public QrFragment() {
-        // Required empty public constructor
-    }
+        public QrFragment() {
+            // Required empty public constructor
+        }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QrFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QrFragment newInstance(String param1, String param2) {
-        QrFragment fragment = new QrFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        @SuppressLint("MissingInflatedId")
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_qr, container, false);
+            scannedTextView = view.findViewById(R.id.textQr);
+
+            scan = view.findViewById(R.id.Scan);
+            scannedTextView.setBackground(null);
+            scan.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IntentIntegrator integrator = IntentIntegrator.forSupportFragment(QrFragment.this);
+
+                    integrator.setOrientationLocked(false);
+                    integrator.setPrompt("Scan QR code");
+                    integrator.setBeepEnabled(false);
+                    integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                    integrator.setCameraId(0);  // Use a specific camera of the device
+                    integrator.initiateScan();
+                }
+            });
+
+            return view;
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if(result != null) {
+                if(result.getContents() == null) {
+                    Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    String scannedContent = result.getContents();
+                    if (scannedContent.startsWith("http")) {
+                        scannedTextView.setText(null);
+                        scannedTextView.setBackground(null);
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(scannedContent));
+                        startActivity(browserIntent);
+                    } else {
+                        scannedTextView.setText(scannedContent);
+                    }
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
+            }
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr, container, false);
-    }
-}
